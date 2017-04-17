836037dd5636fd41f1127c6cc1ee551d29f2fb46
Jose Perdiguero <perdy.hh@gmail.com>
2017-04-17 12:35:24 +0200

    Add build and upgrade requirements

---
+++ build.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+from clinner.run import Main
+
+
+class Build(Main):
+    commands = (
+        'clinner.run.commands.nose.nose',
+        'clinner.run.commands.prospector.prospector',
+        'clinner.run.commands.sphinx.sphinx',
+        'clinner.run.commands.tox.tox',
+        'clinner.run.commands.dist.dist',
+    )
+
+
+def main():
+    return Build().run()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
